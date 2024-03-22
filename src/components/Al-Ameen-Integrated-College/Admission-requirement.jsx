3afc475c1ee3767ed65cbaa96827d67458ce5cc8
Abdulquadri-Mahmud <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2024-03-22 07:24:48 +0100

    updated

--- src/components/Al-Ameen-Integrated-College/Admission-requirement.jsx
+++ src/components/Al-Ameen-Integrated-College/Admission-requirement.jsx
@@ -5,11 +5,11 @@ export default function AdmissionRequirement() {
   return (
     <div>
       <Box pt={7} px={{md: 10, base: 5}}>
-            <Heading fontSize={25}>Who is this for (Admission Requirements)</Heading>
-            <Text pt={3} fontSize={{md: 16, base: 16}} fontWeight={600}>
+            <Heading fontSize={25} fontWeight={500}>Who is this for (Admission Requirements)</Heading>
+            <Text pt={3} fontSize={{md: 16, base: 16}} fontWeight={500}>
                 For parents who wants their kids to have best of both worlds.
             </Text>
-            <UnorderedList pt={3} fontWeight={600} fontSize={{md: 16, base: 16}}>
+            <UnorderedList pt={3} fontWeight={500} fontSize={{md: 16, base: 16}}>
                 <ListItem lineHeight={2}>To be admitted, a child must have completed primary school education (Pry 5 or 6 depending)</ListItem>
                 <ListItem lineHeight={2}>Must already be able to read the Qur'an</ListItem>
                 <ListItem lineHeight={2}>He or she must be at least 9 years old and aged 11 max</ListItem>
