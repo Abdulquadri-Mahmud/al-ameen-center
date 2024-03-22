c2a4131bcd605e89c19385db10b7c9bd935209bd
Abdulquadri-Mahmud <114073342+Abdulquadri-Mahmud@users.noreply.github.com>
2024-03-22 05:52:23 +0100

    updated

--- src/components/ACMQ.jsx
+++ src/components/ACMQ.jsx
@@ -8,23 +8,23 @@ export default function ACMQ() {
         <Flex py={{base: 7, md: 10}} justifyContent={'space-between'} flexWrap={'wrap'} alignItems={'center'} px={{base: 3, md: 5, xl: 10}} bg={useColorModeValue('gray.200', 'gray.700')}>
             <Box width={'100%'} mx={'auto'}>
                 <Heading fontSize={{base:20, md:30}} textAlign={'center'} color={useColorModeValue('green.700', 'white')}>Al-Ameen Centre for <br /> Qur'an Memorization (ACQM)</Heading>
-                <Flex justifyContent={'space-around'} alignItems={'center'} flexWrap={'wrap'} mt={10}>
+                <Flex justifyContent={'space-around'} alignItems={'center'} flexWrap={'wrap'} mt={10} bg={'green.600'} rounded={11}>
                     <Box w={{md:'45%', base: '100%'}} bg={useColorModeValue('gray.100', 'gray.800')} borderRadius={10} maxW={'full'}>
                         <img src={quran} alt="quran" />
                     </Box>
-                    <Box w={{md:'50%', base: '100%'}} mt={{md:0, base:6}}>
+                    <Box w={{md:'50%', base: '100%'}} mt={{md:0, base:6}} px={{base:2, md: 0}} color={'white'}>
                         <Box>
                             <Heading fontSize={25} textAlign={'center'} pt={5} textDecor={'underline'}>-خَيْرُکُم مَنْ تَعَلَّمَ الْقُرْآنَ وَعَلَّمَهُ-</Heading>
                             <Box mt={{base:5, md: 7}}>
-                                <Text fontSize={16} fontWeight={500} lineHeight={2} textAlign={{base: 'center', md: 'start'}}>We, at "Al-Ameen Centre for Qur'an Memorization" are glad to provide an opportunity 
+                                <Text fontSize={16} fontWeight={500} lineHeight={2} textAlign={{base: 'start', md: 'start'}}>We, at "Al-Ameen Centre for Qur'an Memorization" are glad to provide an opportunity 
                                 to support our parents on the challenging journey of getting their children to become protectors 
                                 of the words  of Allah. We prioritize the raising memorizers of the Qur'an who are morally upright
-                                and spiritually sound. And that explains our motto: <span className='text-green-700 font-bold'>“Qur’an & Character”.</span></Text>
+                                and spiritually sound. And that explains our motto: <span className='text-green-700 font-bold bg-white px-2 py-1 rounded'>“Qur’an & Character”.</span></Text>
                             </Box>
                         </Box>
                         <Box mt={6}>
                             <Heading  fontSize={25} textDecor={'underline'} textAlign={'center'}>Key Program Highlights</Heading>
-                            <Flex flexDir={'column'} gap={3} pt={7}>
+                            <Flex flexDir={'column'} gap={3} py={7}>
                                 <Text fontSize={16} fontWeight={500} textAlign={'start'}>We take care to ensure that the basis of the learner’s character development is modelled on the teachings of the Qur’an and Sunnah of prophet </Text>
                                 <Text fontSize={16} fontWeight={500} textAlign={'start'}>We do not neglect traditional Nigerian home trainings taught by our parents to enhance our pupils, as virtuous and confident representatives of Islam both in expression and, in attitude.</Text>
                                 <Text fontSize={16} fontWeight={500} textAlign={'start'}>We do the above in a conducive living environment which encourages the use of Arabic in day-to-day communication as part of setting up the foundations that will enable the understanding of the Quran in future.</Text>
